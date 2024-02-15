fb7c1673b3203c20c55b05963d7394c77a077545
Harshit Halwan <harshithalwan@gmail.com>
2024-02-15 13:32:16 +0530

    deploy config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import photo from './photo1.png'
+import { ContactCard } from './ContactCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div>
+          <h1 className='name'>Harshit Halwan</h1>
+          <h4 className='designation'>Full-Stack Engineer</h4>
+        </div>
+        <img src={photo} className='photo' alt='photo'/>
+      </div>
+      <div className='contactCard'>
+      <ContactCard/>
+      </div>
     </div>
   );
 }
